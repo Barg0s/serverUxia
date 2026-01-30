390f7a94b53fe8560d149a9156a801613a5b9955
DenisFV05 <152627686+DenisFV05@users.noreply.github.com>
2026-01-30 16:51:44 +0100

    de sqlite a MYSQL (con docker)
    hace falta instalar mysql2 y docker

--- database.js
+++ database.js
@@ -1,21 +1,27 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const dialect = process.env.DB_DIALECT || 'sqlite';
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: process.env.DB_DIALECT,
+    logging: false,
+  }
+);
 
-let sequelize;
-if (dialect === 'sqlite') {
-  sequelize = new Sequelize({
-    dialect: 'sqlite',
-    storage: process.env.DB_STORAGE || './db/database.sqlite',
-    logging: false
-  });
-} else {
-  // ejemplo para DATABASE_URL
-  sequelize = new Sequelize(process.env.DATABASE_URL, {
-    dialect,
-    logging: false
-  });
+async function testConnection() {
+  try {
+    await sequelize.authenticate();
+    console.log('✅ Conexión a la base de datos UXIA establecida');
+  } catch (error) {
+    console.error('❌ Error conectando a UXIA MySQL:', error);
+  }
 }
 
-module.exports = sequelize;
+testConnection();
+
+module.exports = { sequelize };
